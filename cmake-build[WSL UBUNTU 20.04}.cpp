bf412ed326b0e763f3b3e0365fd706eff52c00b2
benardnicolas3 <67842599+nicolasbenard3@users.noreply.github.com>
2020-11-16 22:45:07 +0100

    Add files via upload

---
+++ cmake-build[WSL UBUNTU 20.04}.cpp
@@ -0,0 +1,12 @@
+﻿// cmake-build[WSL UBUNTU 20.04}.cpp : définit le point d'entrée de l'application.
+//
+
+#include "cmake-build[WSL UBUNTU 20.04}.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
